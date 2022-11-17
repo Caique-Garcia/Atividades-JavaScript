09623c49391b14ba8eb688270f09c2a728065909
Caique Ferreira <111088915+Caique-Garcia@users.noreply.github.com>
2022-11-17 16:34:49 -0300

    Aulas de JavaScript

---
+++ meu_script.js
@@ -0,0 +1,13 @@
+
+
+var numeroInteiro = -7
+
+var numeroFracionado = 10.75
+
+var texto = 'Curso de JavaScript ' + numeroInteiro + ' texto atoa'
+
+var teste = true
+
+document.write (texto)
+
+console.log (numeroInteiro)
