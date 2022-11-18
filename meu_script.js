7051220ef92cd0afdf7390e27d52777779d2c574
Caique Ferreira <111088915+Caique-Garcia@users.noreply.github.com>
2022-11-18 13:24:05 -0300

    Exercicios JavaScript

--- meu_script.js
+++ meu_script.js
@@ -11,3 +11,27 @@ var teste = true
 document.write (texto)
 
 console.log (numeroInteiro)
+
+var a = 10
+
+var b = 20 
+
+var c = null
+
+document.write('<h1>' + a +'</h1>')
+
+document.write('<h1>' + b +'</h1>')
+
+document.write('<h1>' + c +'</h1>')
+
+c = a 
+
+a = b
+
+b = c 
+
+c = null
+
+document.write('<h1>' + a +'</h1>')
+
+document.write('<h1>' + b +'</h1>')
