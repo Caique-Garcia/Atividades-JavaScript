8f9ee86ca74f57456ca8484ec3baaa5312fedfc5
Caique Ferreira <111088915+Caique-Garcia@users.noreply.github.com>
2022-11-21 17:23:27 -0300

    Atividades do curso de JavaScript

--- meu_script.js
+++ meu_script.js
@@ -45,5 +45,16 @@ if (nota>=media) {
 	document.write('<h1>O aluno foi reprovado.</h1>')
 }
 
+var variavel1 = prompt('Digite algum número: ')
+var variavel2 = prompt('Digite outro número: ')
+
+variavel1 = parseInt(variavel1)
+variavel2 = parseFloat(variavel2)
+
+console.log(variavel1)
+console.log(variavel2)
+
+document.write(variavel1 + variavel2)
+
 
 
