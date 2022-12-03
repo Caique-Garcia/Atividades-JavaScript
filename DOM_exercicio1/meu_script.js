2f9b46b1d34941f1addc54cb876059da13f3cdf0
Caique Ferreira <111088915+Caique-Garcia@users.noreply.github.com>
2022-12-03 09:03:53 -0300

    Exercícios JS

---
+++ DOM_exercicio1/meu_script.js
@@ -0,0 +1,29 @@
+
+function distribui() {
+	
+	var caracter = document.getElementById('entrada').value
+
+	document.getElementById('entrada').value = ''
+
+	caracter.trim()	
+
+	switch(caracter) {
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+			document.getElementById('numeros').value = caracter
+			break;
+
+		default:
+			document.getElementById('letras').value = caracter
+
+	}
+
+}
