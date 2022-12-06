bbc003535be9b385f1e55ab525774e959e674922
Caique Ferreira <111088915+Caique-Garcia@users.noreply.github.com>
2022-12-06 13:15:34 -0300

    Praticando JS DOM - Alterando Classes

---
+++ DOM_exercicios_darkMode/meu_script.js
@@ -0,0 +1,11 @@
+function modificaEstilo(corDeFundo) {
+	document.getElementById('quadrado').style.backgroundColor = corDeFundo
+}
+
+function nightMode() {
+	document.getElementById('container').className = 'night'
+}
+
+function dayMode() {
+	document.getElementById('container').className = 'day'
+}
