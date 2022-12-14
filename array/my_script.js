0a547bb61e921bf63f0be24d1eef3c55319975fe
Caique Ferreira <111088915+Caique-Garcia@users.noreply.github.com>
2022-12-14 12:32:33 -0300

    Adicionando ordenação

--- array/my_script.js
+++ array/my_script.js
@@ -18,3 +18,29 @@ lista_frutas.pop()
 lista_frutas.shift()
 
 console.log(lista_frutas)
+
+/*indexof() traz o indice do elemento no array*/
+/*retorna valor -1 quando elemento não existe no array*/
+console.log(lista_frutas.indexOf('Maçã'))
+
+/*Ordena o array de forma alfanumérica*/
+console.log(lista_frutas.sort())
+
+
+var lista_numeros = Array()
+
+lista_numeros[0] = 17
+lista_numeros[1] = 10
+lista_numeros[2] = 1
+lista_numeros[3] = 8
+lista_numeros[4] = 40
+lista_numeros[5] = 11
+lista_numeros[6] = 2
+
+/*Para ordenar lista de numeros é nesserio add uma função de parametro no metodo sort()*/
+console.log(lista_numeros.sort(ordenaNum))
+
+/*função de parametro para ordernar numeros*/
+function ordenaNum(a,b) {
+	return a - b
+}
