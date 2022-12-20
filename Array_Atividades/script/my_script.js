ca00449d3373911f4a63be8a81b126fc26fc3304
Caique Ferreira <111088915+Caique-Garcia@users.noreply.github.com>
2022-12-20 08:30:22 -0300

    Percorrendo Array com 'for' e 'while'

---
+++ Array_Atividades/script/my_script.js
@@ -0,0 +1,24 @@
+
+var listaNomes = Array()
+
+listaNomes[0] = 'Caique'
+listaNomes[1] = 'Caio'
+listaNomes[2] = 'Rafael'
+listaNomes[3] = 'Lucas'
+listaNomes[4] = 'Tales'
+listaNomes[5] = 'Danilo' 
+listaNomes[6] = 'Rafaela'
+
+console.log(listaNomes)
+
+for(var i in listaNomes){
+	document.write('Codigo: ' + i + ' Cliente: '+ listaNomes[i] + '<br />')
+}
+
+document.write('<br />')
+i = 0
+
+while (i < listaNomes.length) {
+	document.write('Codigo: ' + i + ' Cliente: '+ listaNomes[i] + '<br />')
+	i++
+}
